04d041269f3214087e2884e51c1d47cbe0240a6b
zoha1985 <shumsuzzuha@gmail.com>
2021-06-06 21:13:31 +0600

    zoha

---
+++ screens/Creative/creative.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const creative = () => {
+    return (
+        <div>
+            <h3>This is a news Editor </h3>
+       
+        </div>
+    );
+};
+
+export default creative;
